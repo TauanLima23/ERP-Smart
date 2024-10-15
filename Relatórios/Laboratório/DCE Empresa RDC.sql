f9337fc6203624c033051c8d1a02aede14dc2bd2
Tauan Lima <contato@tauanlima.com.br>
2024-10-15 16:06:40 -0300

    Final - Insuficiencia Exame

--- Relatórios/Laboratório/DCE Empresa RDC.sql
+++ Relatórios/Laboratório/DCE Empresa RDC.sql
@@ -1,15 +1,14 @@
-SELECT 
-    CASE 
-        WHEN SMK.SMK_TERC = 'S' THEN EMP.EMP_NOME_FANTASIA 
-        ELSE CFG.CFG_EMP 
-    END 
-FROM 
-    RCL 
-    CROSS JOIN CFG
-    INNER JOIN SMK ON SMK.SMK_COD = RCL.RCL_COD 
-    LEFT JOIN EMP ON SMK.SMK_TERC_EMP_COD = EMP.EMP_COD 
-    LEFT JOIN PSV ON PSV.PSV_COD = EMP.EMP_RESP_TECNICO 
-WHERE  
-    RCL.RCL_PAC = :paciente 
-    AND RCL.RCL_DTHR = :dtrcl 
-    AND RCL.RCL_COD = :servico
+SELECT
+    CASE
+        WHEN EMP.EMP_NOME_FANTASIA IS NOT NULL THEN '| Realizado por: ' + EMP.emp_nome_fantasia
+        ELSE ''
+    end
+FROM
+    RCL
+    INNER JOIN SMK ON (SMK.SMK_COD = RCL.RCL_COD)
+    LEFT JOIN EMP ON (RCL.RCL_APARELHO = EMP.EMP_B2B_ELB_COD)
+    LEFT JOIN PSV ON (PSV.PSV_COD = EMP.EMP_RESP_TECNICO)
+WHERE
+    rcl_pac = :paciente
+    and rcl_dthr = :dtrcl
+    and rcl_cod = :servico
